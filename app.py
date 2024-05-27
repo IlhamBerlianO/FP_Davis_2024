00750b66d4395ce941e06990dc14987d65ae0c55
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-05-27 19:11:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,15 +103,15 @@ col1, col2, col3 = st.columns([3,1,1])
     # Tabs
     col1.subheader('Tabs')
     col1.code(
-# Insert containers separated into tabs:
->>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
->>> tab1.write("this is tab 1")
->>> tab2.write("this is tab 2")
-
-# You can also use "with" notation:
->>> with tab1:
->>>   st.radio('Select one:', [1, 2])
-)
+     # Insert containers separated into tabs:
+     >>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+     >>> tab1.write("this is tab 1")
+     >>> tab2.write("this is tab 2")
+     
+     # You can also use "with" notation:
+     >>> with tab1:
+     >>>   st.radio('Select one:', [1, 2])
+    )
 
     # Control flow
     col1.subheader('Control flow')
