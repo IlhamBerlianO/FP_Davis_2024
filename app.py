5430c006c0c83f868f9bc3ddc250882156c79eab
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-05-27 19:09:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def cs_body():
     
     # Display text
     col1.subheader('Display text')
-    col1.code(
+    col1.code('''
      st.text('Fixed width text'),
      st.markdown('_Markdown_'), # see #*
      st.caption('Balloons. Hundreds of them...'),
@@ -62,6 +62,7 @@ def cs_body():
      # st.subheader('My sub'),
      st.code('for i in range(8): foo()'),
      * optional kwarg unsafe_allow_html = True
+     '''
     )
 
     # Display data
