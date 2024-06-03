70123a5777cfe7f3dfdb04f4558c00c190989d18
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-06-04 06:54:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def main():
     cs_sidebar()
     cs_body()
 
+    return None
+
 def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
