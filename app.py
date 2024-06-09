81ec49765e08921eb722c9e1060491a1fe5a2445
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-06-09 16:31:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from pathlib import Path
-import base64
 import matplotlib.pyplot as plt
 import pandas as pd
 from gtts import gTTS
@@ -13,10 +12,10 @@ def main():
     cs_body(data_dipilih)
     return None
 
-# def img_to_bytes(img_path):
-#     img_bytes = Path(img_path).read_bytes()
-#     encoded = base64.b64encode(img_bytes).decode()
-#     return encoded
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
     
 ##########################
 # SIDEBAR
@@ -63,8 +62,75 @@ def cs_body(data_dipilih):
         
 
     elif data_dipilih == "Scrapping IMDB":
-        st.subheader("Scrapping IMDB")
+        # Judul aplikasi
+        st.title("Scrapping IMDB")
         st.write("Ini adalah contoh konten yang akan ditampilkan jika Database Dump_AW dipilih.")
+        
+        # Deskripsi
+        st.write(f'Visualisasi data film dengan menggunakan data dari www.imdb.com')
+        
+        # Membaca file excel
+        baca = pd.read_excel("scrapping_imdb/top_picks_data.xlsx")
+        
+        # Ambil data film
+        title = baca['Title'].tolist()
+        gross = baca['Gross_us'].tolist()
+        summary = baca['Summary'].tolist()
+        image = baca['Image'].tolist()
+        rating = baca['Rating'].tolist()
+        genre = baca['Genres'].tolist()
+        runtime = baca['runtime'].tolist()
+        
+        # # Plot grafik
+        plt.figure(figsize=(10, 6))
+        plt.barh(title, gross, color='#398bff')
+        plt.xlabel('Pendapatan ($)')
+        plt.ylabel('Judul Film')
+        plt.title('Grafik Film Top Picks')
+        plt.gca().invert_yaxis() 
+        st.pyplot(plt)
+        
+        # Judul aplikasi
+        st.title('Nama Film')
+        
+        # Pilih saham dari dropdown
+        selected_stock = st.selectbox('Pilih Film:', title)
+        
+        # Temukan indeks saham yang dipilih di daftar perusahaan
+        index = title.index(selected_stock)
+        
+        # Tampilkan detail perusahaan yang dipilih
+        st.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid'>](https://streamlit.io/)'''.format(img_to_bytes(f"gambar/{image[index]}")), unsafe_allow_html=True)
+        st.write(f'Judul Film: {title[index]}')
+        st.write(f'Rating: ${rating[index]}')
+        st.write(f'Genre: ${genre[index]}')
+        st.write(f'Waktu: ${runtime[index]} Menit')
+        st.write(f'Penjelasan Singkat:')
+        
+        # Inisialisasi objek Translator
+        translator = GoogleTranslator(source='en', target='id')
+        
+        # Tambahkan tombol untuk membaca deskripsi perusahaan
+        if st.button("Translate ke Indonesia"):
+            # Translate deskripsi dari bahasa Inggris ke bahasa Indonesia
+            summary_id = translator.translate(summary[index])
+            summary[index] = summary_id
+        
+        st.write(summary[index])
+        
+        # Fungsi untuk merubah teks deskripsi menjadi suara
+        def text_to_speech(text):
+            tts = gTTS(text=text, lang='en')  # Menggunakan gTTS untuk mengonversi teks ke suara dalam bahasa Inggris
+            speech = io.BytesIO()
+            tts.write_to_fp(speech)
+            return speech.getvalue()
+        
+        # Tambahkan tombol untuk membaca deskripsi perusahaan
+        if st.button("Baca Deskripsi"):
+            speech_bytes = text_to_speech(summary[index])
+            st.audio(speech_bytes, format='audio/mp3')
+        
+        st.write(f'Created by Ilham Berlian Oktavio')
 
     else:
         st.write("Data yang dipilih tidak tersedia")
